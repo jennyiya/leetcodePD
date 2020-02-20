52aa59a112a4a40380b253d2fb784d3d196bd491
jenny <jenny@outlook.com>
2020-02-20 21:04:31 +0800

    modified

--- 11/solution1.go
+++ 11/solution1.go
@@ -4,10 +4,13 @@ func maxArea(height []int) int {
     var res,h int
     i:=0
     j:=len(height)-1
+    if i ==j{
+        return 0
+    }
     for i<j{
          h = int(math.Min(float64(height[i]),float64(height[j])))
          
-        res = int(math.Max(float64(res), float64(h*(j-1))))
+        res = int(math.Max(float64(res), float64(h*(j-i))))
         
         if height[i]<height[j]{
             i++
