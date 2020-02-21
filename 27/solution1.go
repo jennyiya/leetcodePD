d74b26e6f71dc55dea28e8b70aceb242f1b85931
jenny <jenny@outlook.com>
2020-02-21 23:29:29 +0800

    modified27

--- 27/solution1.go
+++ 27/solution1.go
@@ -1,12 +1,25 @@
-package solution1
-
+package main
+import("fmt")
 func removeElement(nums []int, val int) int {
-    j := 0
+	j := 0
+	if len(nums)==0{
+		return 0
+	}
+	var res []int
     for i:=0;i<len(nums);i++{
         if nums[i] != val{
-           nums[j] = nums[i]
+		   //res[j] = nums[i]
+		   res = append(res,nums[i])
            j++
         }
-    }
+	}
+	fmt.Printf("res is%v",res)
     return j
+}
+
+func main(){
+	//var res []int
+	a :=[]int{0,1,2,2,3,0,4,2}
+	l := removeElement(a,2)
+	fmt.Printf("the arr is %v,the len is %v",a,l)
 }
